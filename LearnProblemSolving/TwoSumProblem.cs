1b36e34b1fc6f6f8d606a803d30e8b63df2aa439
Jaydeep Dave <jaydeep.dave05@hotmail.com>
2023-09-14 20:18:13 -0400

    Add project files.

---
+++ LearnProblemSolving/TwoSumProblem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnProblemSolving
+{
+    internal class TwoSumProblem
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int arrLength = nums.Length;
+            //Validation
+            if (nums == null || arrLength < 2)
+            {
+                return Array.Empty<int>();
+            }
+            //Logic
+            for (int i = 0; i < arrLength; i++)
+            {
+                for (int j = i + 1; j < arrLength; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+
+            }
+
+            return Array.Empty<int>();
+
+        }
+    }
+}
